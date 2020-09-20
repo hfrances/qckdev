438622a9af640c313f21b564cef11f3d01aa67f2
Héctor FRANCES <hector.frances@hmy-group.com>
2020-09-20 15:43:34 +0200

    Added "qckdev.Linq.Queriable.LeftJoin" tests.

---
+++ qckdevTest/TestObjects/Entities/Child.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace qckdevTest.TestObjects.Entities
+{
+    class Child
+    {
+
+        public Guid Id { get; set; }
+        public int Line { get; set; }
+        public string Description { get; set; }
+        public string Remarks { get; set; }
+
+
+        public Parent Parent { get; set; }
+
+    }
+}
---
+++ qckdevTest/TestObjects/TestDbContext.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace qckdevTest.TestObjects
+{
+    class TestDbContext : DbContext
+    {
+
+        public TestDbContext(DbContextOptions<TestDbContext> options) : base(options)
+        { }
+
+        public DbSet<Entities.Parent> Parents { get; set; }
+        public DbSet<Entities.Child> Childs { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Entities.Parent>(builder =>
+            {
+                builder.HasKey(x => x.Id);
+                builder.HasIndex(x => x.Number).IsUnique();
+                builder.Property(x => x.Name).IsRequired();
+                builder
+                    .HasMany(x => x.Childs)
+                    .WithOne(x => x.Parent)
+                    .OnDelete(DeleteBehavior.Cascade);
+            });
+
+            modelBuilder.Entity<Entities.Child>(builder =>
+            {
+                builder.HasKey(x => new { x.Id, x.Line });
+                builder.Property(x => x.Description).IsRequired();
+            });
+        }
+
+        public static TestDbContext CreateInstance()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<TestDbContext>();
+
+            optionsBuilder.UseInMemoryDatabase("TestDb");
+            return new TestDbContext(optionsBuilder.Options);
+        }
+
+    }
+}
