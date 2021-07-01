18623f4c79b839b7f2dfa031baf20bc80a702008
Héctor Francés <32682445+hfrances@users.noreply.github.com>
2021-07-01 16:57:07 +0200

    Create qckdev.Net.Http project.

---
+++ qckdevTest/Net/Http/HttpClientTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using qckdev.Linq;
+
+namespace qckdevTest.Net.Http
+{
+
+    [TestClass]
+    public class HttpClientTest
+    {
+
+        [TestMethod]
+        public void FetchAsync()
+        {
+            Assert.Inconclusive();
+        }
+
+    }
+}
